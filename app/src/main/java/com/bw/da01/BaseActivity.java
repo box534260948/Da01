962005c29ddd212c8b1b8e79e321151f70049cce
buxx <534260948@qq.com>
2019-06-06 15:50:06 +0800

    Initial commit

---
+++ app/src/main/java/com/bw/da01/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.bw.da01;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+    }
+}
---
+++ app/src/main/java/com/bw/da01/MyApplication.java
@@ -0,0 +1,17 @@
+package com.bw.da01;
+
+import android.app.Application;
+
+import com.uuzuche.lib_zxing.activity.ZXingLibrary;
+
+/**
+ * Create by ysn on 2019/6/6
+ */
+public class MyApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ZXingLibrary.initDisplayOpinion(this);
+    }
+}
